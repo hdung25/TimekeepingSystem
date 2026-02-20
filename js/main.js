772adc52e5220e04b3552382ce3d130e838ca3d3
Hà Huy Dũng <hahuyd25@gmail.com>
2026-02-20 11:50:09 +0700

    fix: Add Thong Ke to dynamic sidebar menuItems

--- js/main.js
+++ js/main.js
@@ -468,6 +468,15 @@ function renderSidebar() {
             event: "switchTab('tab-maintenance', event); return false;",
             icon: '<polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2-2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line>',
             roles: ['admin']
+        },
+        // NEW: Analytics
+        {
+            name: 'Thống Kê',
+            link: '#',
+            id: 'nav-analytics',
+            event: "switchTab('tab-analytics', event); return false;",
+            icon: '<path d="M21 12a9 9 0 1 1-9-9"></path><path d="M21 3v9h-9"></path>',
+            roles: ['admin']
         }
     ];
 
